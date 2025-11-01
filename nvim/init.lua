a5e9b705ea8e056bb1359b885d36976e928939e4
Joshua Snook <contact@jsnook.co.uk>
2025-11-01 20:46:26 +0000

    feat(Nvim): added line numbers and relative line numbers
    Signed-off-by: Joshua Snook <contact@jsnook.co.uk>

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,3 @@
-require("config.lazy")
 
 
 -- Set tab to be 4 spaces instead of a tab
@@ -6,5 +5,10 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.softtabstop = 4
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+require("config.lazy")
 
 
